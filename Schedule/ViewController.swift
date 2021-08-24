e03afafc5f2dede4facc94dae5f20480e9819f57
Pavel Shatunov <shatuman@ya.ru>
2021-08-24 15:17:27 +0300

    Initial Commit

---
+++ Schedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by Павел Шатунов on 24.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
